cd31785aaecc3fa6e41aad300fcb8109ecc4a439
Dharmin Patel <dharminpatel99@gmail.com>
2021-08-11 12:17:07 +0530

    Implemented create action

--- app/javascript/src/App.jsx
+++ app/javascript/src/App.jsx
@@ -3,6 +3,7 @@ import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
 import { setAuthHeaders } from "./apis/axios";
 import { initializeLogger } from "./common/logger";
 import Dashboard from "./components/Dashboard";
+import CreateTask from "./components/Tasks/CreateTask";
 
 const App = () => {
   const [loading, setLoading] = useState(true);
@@ -17,8 +18,9 @@ const App = () => {
   return (
     <Router>
       <Switch>
-        <Route exact path="/" render={() => <div>Home</div>} />
-        <Route exact path="/about" render={() => <div>About</div>} />
+        <Route exact path="/" component={Dashboard} />
+        {/*<Route exact path="/about" render={() => <div>About</div>} /> */}
+        <Route exact path="/tasks/create" component={CreateTask} />
         <Route exact path="/dashboard" component={Dashboard} />
       </Switch>
     </Router>
