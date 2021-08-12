bb3b815de794e2c6eafe5f4c567801934b67902a
Dharmin Patel <dharminpatel99@gmail.com>
2021-08-12 16:23:43 +0530

    Added tests for User model

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,8 +1,12 @@
-ENV['RAILS_ENV'] ||= 'test'
+# frozen_string_literal: true
+
+ENV["RAILS_ENV"] ||= "test"
 require_relative "../config/environment"
 require "rails/test_help"
 
 class ActiveSupport::TestCase
+  include ActionView::Helpers::TranslationHelper
+
   # Run tests in parallel with specified workers
   parallelize(workers: :number_of_processors)
 
@@ -11,3 +15,4 @@ class ActiveSupport::TestCase
 
   # Add more helper methods to be used by all tests here...
 end
+
