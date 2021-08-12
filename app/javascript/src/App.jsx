bd5d94106437e76605e8538b367eddd68c7626ec
Dharmin Patel <dharminpatel99@gmail.com>
2021-08-12 15:52:34 +0530

    Added login feature

--- app/javascript/src/App.jsx
+++ app/javascript/src/App.jsx
@@ -1,37 +1,53 @@
 import React, { useEffect, useState } from "react";
 import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
+import { either, isEmpty, isNil } from "ramda";
 import { ToastContainer } from "react-toastify";
-
-import { setAuthHeaders, registerIntercepts } from "./apis/axios";
-import { initializeLogger } from "./common/logger";
-import Dashboard from "./components/Dashboard";
-import CreateTask from "./components/Tasks/CreateTask";
+import { registerIntercepts, setAuthHeaders } from "apis/axios";
+import { initializeLogger } from "common/logger";
+import Dashboard from "components/Dashboard";
+import CreateTask from "components/Tasks/CreateTask";
 import ShowTask from "components/Tasks/ShowTask";
 import EditTask from "components/Tasks/EditTask";
-import Signup from "./components/Authentication/Signup";
+import Login from "components/Authentication/Login";
+import Signup from "components/Authentication/Signup";
+import PrivateRoute from "components/Common/PrivateRoute";
+import { getFromLocalStorage } from "helpers/storage";
+import PageLoader from "components/PageLoader";
 
 const App = () => {
   const [loading, setLoading] = useState(true);
+  const authToken = getFromLocalStorage("authToken");
+  const isLoggedIn = !either(isNil, isEmpty)(authToken) && authToken !== "null";
 
   useEffect(() => {
-    initializeLogger();
     registerIntercepts();
+    initializeLogger();
     setAuthHeaders(setLoading);
-    // logger.info("Never use console.log");
-    // logger.error("Never use console.error");
   }, []);
+
+  if (loading) {
+    return (
+      <div className="h-screen">
+        <PageLoader />
+      </div>
+    );
+  }
 
   return (
     <Router>
       <ToastContainer />
       <Switch>
-        <Route exact path="/signup" component={Signup} />
-        <Route exact path="/" component={Dashboard} />
-        {/*<Route exact path="/about" render={() => <div>About</div>} /> */}
+        <Route exact path="/tasks/:slug/show" component={ShowTask} />
         <Route exact path="/tasks/:slug/edit" component={EditTask} />
         <Route exact path="/tasks/create" component={CreateTask} />
-        <Route exact path="/dashboard" component={Dashboard} />
-        <Route exact path="/tasks/:slug/show" component={ShowTask} />
+        <Route exact path="/signup" component={Signup} />
+        <Route exact path="/login" component={Login} />
+        <PrivateRoute
+          path="/"
+          redirectRoute="/login"
+          condition={isLoggedIn}
+          component={Dashboard}
+        />
       </Switch>
     </Router>
   );
