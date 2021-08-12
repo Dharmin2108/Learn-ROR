4210aab0df7105017ca732b776248b7796b01a29
Dharmin Patel <dharminpatel99@gmail.com>
2021-08-12 16:14:50 +0530

    Added creator id to tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class TasksController < ApplicationController
-  before_action :authenticate_user_using_x_auth_token, except: [:new, :edit]
+  before_action :authenticate_user_using_x_auth_token
   before_action :load_task, only: [:show, :update, :destroy]
 
   def show
@@ -33,11 +33,11 @@ class TasksController < ApplicationController
   end
 
   def create
-    task = Task.new(task_params)
-    if task.save
+    @task = Task.new(task_params.merge(creator_id: @current_user.id))
+    if @task.save
       render status: :ok, json: { notice: t("successfully_created", entity: "Task") }
     else
-      errors = task.errors.full_messages.to_sentence
+      errors = @task.errors.full_messages.to_sentence
       render status: :unprocessable_entity, json: { errors: errors }
     end
   end
